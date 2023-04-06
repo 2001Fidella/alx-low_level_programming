3cb1cc1b14af0b9d512cdcbf8e42f8e792b98d5e
Fidella <abafidela@gmail.com>
2023-04-05 18:18:19 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -55,7 +55,7 @@ int _strlen_recursion(char *s);
 int is_palindrome(char *s)
 {
 if (*s == 0)
-        return (1);
+	return (1);
 return (check_pal(s, 0, _strlen_recursion(s)));
 }
 /**
@@ -67,7 +67,7 @@ return (check_pal(s, 0, _strlen_recursion(s)));
 int _strlen_recursion(char *s)
 {
 if (*s == '\0')
-        return (0);
+	return (0);
 return (1 + _strlen_recursion(s + 1));
 }
 
